32dfe4f6851cc194b98cd26b302ba53ea4fdf7ec
Zsolt Kazsi <kzs@gmx.ch>
2013-01-05 18:23:36 +0100

    9 jav

--- python/9.py
+++ python/9.py
@@ -1,5 +1,6 @@
-for a in range(1, 1000):
-    for b in range(1, 1000):
-        c = 1000 - a - b
-        if a**2 + b**2 == c**2:
-            print(a,b,c, a*b*c)
+def problem9(n):
+    for a in range(1, n):
+        for b in range(1, n-1):
+            c = n - a - b
+            if a**2 + b**2 == c**2:
+                print(a,b,c, a*b*c)
